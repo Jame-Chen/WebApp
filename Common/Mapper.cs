c952fe102b25b9cd9e01fd8f3b91840a0de23063
Jame-Chen <931863353@qq.com>
2019-03-11 11:16:46 +0800

    update

---
+++ Common/Mapper.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+    public class Mapper
+    {
+        public static D Mapper<D, S>(S s)
+        {
+            D d = Activator.CreateInstance<D>();
+            try
+            {
+                var sType = s.GetType();
+                var dType = typeof(D);
+                foreach (PropertyInfo sP in sType.GetProperties())
+                {
+                    foreach (PropertyInfo dP in dType.GetProperties())
+                    {
+                        if (dP.Name == sP.Name)
+                        {
+                            dP.SetValue(d, sP.GetValue(s));
+                            break;
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+            return d;
+        }
+    }
+}
