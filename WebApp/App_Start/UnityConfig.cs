6753bcd5b79dc22c0ec56180e63bee4df70b5fcb
Jame-Chen <931863353@qq.com>
2019-07-13 23:22:54 +0800

    update

--- WebApp/App_Start/UnityConfig.cs
+++ WebApp/App_Start/UnityConfig.cs
@@ -1,13 +1,14 @@
-﻿ //TT模板命名空间
-	//生成的文件后缀名为.cs
+﻿//TT模板命名空间
+//生成的文件后缀名为.cs
 
 //使用TT模板自动生成的代码片段
 using System.Web.Mvc;
 using Unity;
 using Unity.Mvc5;
 using IBLL;
-using BLL;
-
+using Microsoft.Practices.Unity;
+using Microsoft.Practices.Unity.Configuration;
+using System.Configuration;
 
 namespace EFMVCApp
 {
@@ -15,33 +16,15 @@ namespace EFMVCApp
     {
         public static void RegisterComponents()
         {
-			var container = new UnityContainer();
+            var container = new UnityContainer();
 
             // register all your components with the container here
             // it is NOT necessary to register your controllers
 
             // e.g. container.RegisterType<ITestService, TestService>();
-			            container.RegisterType<IFileUploaderService, FileUploaderService>();
-			            container.RegisterType<ISysAnnouncementService, SysAnnouncementService>();
-			            container.RegisterType<ISysDepartmentService, SysDepartmentService>();
-			            container.RegisterType<ISysDocumentSysDepartmentService, SysDocumentSysDepartmentService>();
-			            container.RegisterType<ISysDocumentSysPersonService, SysDocumentSysPersonService>();
-			            container.RegisterType<ISysEmailService, SysEmailService>();
-			            container.RegisterType<ISysEmailTempService, SysEmailTempService>();
-			            container.RegisterType<ISysExceptionService, SysExceptionService>();
-			            container.RegisterType<ISysFieldService, SysFieldService>();
-			            container.RegisterType<ISysLogService, SysLogService>();
-			            container.RegisterType<ISysMenuService, SysMenuService>();
-			            container.RegisterType<ISysMenuSysOperationService, SysMenuSysOperationService>();
-			            container.RegisterType<ISysMenuSysRoleSysOperationService, SysMenuSysRoleSysOperationService>();
-			            container.RegisterType<ISysMessageService, SysMessageService>();
-			            container.RegisterType<ISysMessageTempService, SysMessageTempService>();
-			            container.RegisterType<ISysNoticeService, SysNoticeService>();
-			            container.RegisterType<ISysOperationService, SysOperationService>();
-			            container.RegisterType<ISysPersonService, SysPersonService>();
-			            container.RegisterType<ISysRoleService, SysRoleService>();
-			            container.RegisterType<ISysRoleSysPersonService, SysRoleSysPersonService>();
-			    
+
+            UnityConfigurationSection config = (UnityConfigurationSection)ConfigurationManager.GetSection(UnityConfigurationSection.SectionName);
+            config.Configure(container, "defaultContainer");
             DependencyResolver.SetResolver(new UnityDependencyResolver(container));
         }
     }
